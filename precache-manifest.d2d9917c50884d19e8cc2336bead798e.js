67965a0d6d8db19b19463646133dcf08a1a33c59
Sergey Startsev <sergey.starcev@imeretinka.ru>
2018-10-19 16:17:01 +0300

    Updates

---
+++ precache-manifest.d2d9917c50884d19e8cc2336bead798e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e39d4eb1b6944ed8784",
+    "url": "/timeclicker/static/js/runtime~main.5e39d4eb.js"
+  },
+  {
+    "revision": "7ed6ab3c096b6db33f5b",
+    "url": "/timeclicker/static/js/main.7ed6ab3c.chunk.js"
+  },
+  {
+    "revision": "f22bf7c39704f5872971",
+    "url": "/timeclicker/static/js/1.f22bf7c3.chunk.js"
+  },
+  {
+    "revision": "7ed6ab3c096b6db33f5b",
+    "url": "/timeclicker/static/css/main.84b635bc.chunk.css"
+  },
+  {
+    "revision": "5c078fd8144421a03a06177b6c80644c",
+    "url": "/timeclicker/index.html"
+  }
+];
