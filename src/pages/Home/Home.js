46f0a5898a592e2f7c78f94331bdfb7da2513ced
Sergey Startsev <sergey.starcev@imeretinka.ru>
2018-11-21 10:54:17 +0300

    Добавил CurrentTask на главную страницу

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { PageTemplate } from '../../components/PageTemplate/PageTemplate';
 
+import CurrentTask from '../../components/container/CurrentTask/CurrentTask';
+
 import './Home.css';
 
 export const Home = () => (
   <PageTemplate>
-    <div className="Home">
-      <h1>Time Clicker</h1>
-    </div>
+    <h1>Time Clicker</h1>
+    <CurrentTask />
   </PageTemplate>
 );
