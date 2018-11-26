8adb1a098acc7890d9743e28bb888977dddb5f35
Sergey Startsev <sergey.starcev@imeretinka.ru>
2018-11-26 15:33:12 +0300

    стили

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -5,13 +5,11 @@ import './MainMenu.css';
 
 export const MainMenu = () => (
   <nav className="MainMenu">
-    <div className="MainMenu-wrap">
-      <NavLink exact activeClassName="MainMenu-item_active" className="MainMenu-item" to="/">
-        Главная
-      </NavLink>
-      <NavLink className="MainMenu-item" activeClassName="MainMenu-item_active" to="/tasks">
-        Задачи
-      </NavLink>
-    </div>
+    <NavLink exact activeClassName="MainMenu-item_active" className="MainMenu-item" to="/">
+      Главная
+    </NavLink>
+    <NavLink className="MainMenu-item" activeClassName="MainMenu-item_active" to="/tasks">
+      Задачи
+    </NavLink>
   </nav>
 );
